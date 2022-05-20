d63966a14a2a71b3e272f01dee3ccafb45979d6d
Jack Woodrow <jackwoodrow99@gmail.com>
2022-05-20 09:47:34 -0400

    Added case code

--- src/Playground.elm
+++ src/Playground.elm
@@ -60,6 +60,72 @@ divide e f =
 -- main =
 --   Html.text <| String.fromFloat <| add 5 <| multiply 10 <| divide 30 10
 
+-- main =
+--   escapeEarth 1 6.7 "low"
+--     |> Html.text
+
+-- weekday dayInNumber =
+--   if dayInNumber == 0 then
+--     "Sunday"
+
+--   else if dayInNumber == 1 then
+--     "Monday"
+
+--   else if dayInNumber == 2 then
+--     "Tuesday"
+
+--   else if dayInNumber == 3 then
+--     "Wednesday"
+
+--   else if dayInNumber == 4 then
+--     "Thursday"
+
+--   else if dayInNumber == 5 then
+--     "Friday"
+
+--   else if dayInNumber == 6 then
+--     "Saturday"
+
+--   else
+--     "Unknown day"
+
+weekday dayInNumber =
+  case dayInNumber of
+    0 ->
+      "Sunday"
+    1 ->
+      "Monday"
+    2 ->
+      "Tuesday"
+    3 ->
+      "Wednesday"
+    4 ->
+      "Thursday"
+    5 ->
+      "Friday"
+    6 ->
+      "Saturday"
+    _ ->
+      "Unknown day"
+
+hashtag dayInNumber =
+  case weekday dayInNumber of
+    "Sunday" ->
+      "#SinDay"
+    "Monday" ->
+      "#MondayBlues"
+    "Tuesday" ->
+      "#TakeMeBackTuesday"
+    "Wednesday" ->
+      "#HumpDay"
+    "Thursday" ->
+      "#ThrowbackThursday"
+    "Friday" ->
+      "#FlashbackFriday"
+    "Saturday" ->
+      "#Caturday"
+    _ ->
+      "#Whatever"
+
 main =
-  escapeEarth 1 6.7 "low"
-    |> Html.text
+  Html.text <| hashtag 5
